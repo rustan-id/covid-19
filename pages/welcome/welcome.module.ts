882acac68ffe83a7e66f61ee100a605ffd50c922
rustan-id <rustan1987@gmail.com>
2021-01-20 16:31:01 +0700

    2 files modified

--- pages/welcome/welcome.module.ts
+++ pages/welcome/welcome.module.ts
@@ -8,23 +8,18 @@ import { WelcomePageRoutingModule } from "./welcome-routing.module";
 
 import { WelcomePage } from "./welcome.page";
 
-@NgModule({
-  imports: [CommonModule, FormsModule, IonicModule, WelcomePageRoutingModule],
-  declarations: [WelcomePage]
-})
-export class WelcomePageModule {}
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { FormsModule } from "@angular/forms";
-
-import { IonicModule } from "@ionic/angular";
-
-import { WelcomePageRoutingModule } from "./welcome-routing.module";
-
-import { WelcomePage } from "./welcome.page";
+import { MatExpansionModule } from "@angular/material/expansion";
+import { MatStepperModule } from "@angular/material/stepper";
 
 @NgModule({
-  imports: [CommonModule, FormsModule, IonicModule, WelcomePageRoutingModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    WelcomePageRoutingModule,
+    MatExpansionModule,
+    MatStepperModule
+  ],
   declarations: [WelcomePage]
 })
 export class WelcomePageModule {}
